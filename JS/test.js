91cab0a472d4949fa29df59ee76a2ba5076f871b
srikar chavali <chavalisrikar@gmail.com>
2022-01-09 20:31:52 +0000

    Added new sounds and styling

--- JS/test.js
+++ JS/test.js
@@ -7,9 +7,16 @@ playA.addEventListener("click", ()=>{
 })
 window.addEventListener("keydown", (e)=>{
     if(e.keyCode == 65){
-        audioA.play();  
+        audioA.play();
+        playA.style.background = "rgba(148, 150, 150, 0.8)";
+        playA.style.top = "5px";
+        window.addEventListener("keyup", (e)=>{
+            playA.style.background = "white";
+            playA.style.top = "0";  
+        })  
     }
 })
+
 
 playS = document.getElementById("btnS");
 audioS = document.getElementById("audioS");
@@ -19,7 +26,13 @@ playS.addEventListener("click", ()=>{
 })
 window.addEventListener("keydown", (e)=>{
     if(e.keyCode == 83){
-        audioS.play();  
+        audioS.play();
+        playS.style.background = "rgb(46, 45, 45)";
+        playS.style.top = "5px";
+        window.addEventListener("keyup", (e)=>{
+            playS.style.background = "black";
+            playS.style.top = "0";   
+        })
     }
 })
 
@@ -31,7 +44,13 @@ playD.addEventListener("click", ()=>{
 })
 window.addEventListener("keydown", (e)=>{
     if(e.keyCode == 68){
-        audioD.play();  
+        audioD.play();
+        playD.style.background = "rgb(46, 45, 45)";
+        playD.style.top = "5px";
+        window.addEventListener("keyup", (e)=>{
+            playD.style.background = "black";
+            playD.style.top = "0";   
+        })  
     }
 })
 
@@ -43,7 +62,13 @@ playF.addEventListener("click", ()=>{
 })
 window.addEventListener("keydown", (e)=>{
     if(e.keyCode == 70){
-        audioF.play();  
+        audioF.play();
+        playF.style.background = "rgb(46, 45, 45)";
+        playF.style.top = "5px";
+        window.addEventListener("keyup", (e)=>{
+            playF.style.background = "black";
+            playF.style.top = "0";   
+        })  
     }
 })
 
@@ -55,7 +80,13 @@ playJ.addEventListener("click", ()=>{
 })
 window.addEventListener("keydown", (e)=>{
     if(e.keyCode == 74){
-        audioJ.play();  
+        audioJ.play();
+        playJ.style.background = "rgb(46, 45, 45)";
+        playJ.style.top = "5px";
+        window.addEventListener("keyup", (e)=>{
+            playJ.style.background = "black";
+            playJ.style.top = "0";   
+        })  
     }
 })
 
@@ -67,7 +98,13 @@ playK.addEventListener("click", ()=>{
 })
 window.addEventListener("keydown", (e)=>{
     if(e.keyCode == 75){
-        audioK.play();  
+        audioK.play();
+        playK.style.background = "rgb(46, 45, 45)";
+        playK.style.top = "5px";
+        window.addEventListener("keyup", (e)=>{
+            playK.style.background = "black";
+            playK.style.top = "0";   
+        })  
     }
 })
 
@@ -79,7 +116,13 @@ playL.addEventListener("click", ()=>{
 })
 window.addEventListener("keydown", (e)=>{
     if(e.keyCode == 76){
-        audioF.play();  
+        audioF.play();
+        playL.style.background = "rgb(46, 45, 45)";
+        playL.style.top = "5px";
+        window.addEventListener("keyup", (e)=>{
+            playL.style.background = "black";
+            playL.style.top = "0";   
+        })  
     }
 })
 
@@ -91,7 +134,13 @@ play.addEventListener("click", ()=>{
 })
 window.addEventListener("keydown", (e)=>{
     if(e.keyCode == 186){
-        audio.play();  
+        audio.play();
+        play.style.background = "rgb(46, 45, 45)";
+        play.style.top = "5px";
+        window.addEventListener("keyup", (e)=>{
+            play.style.background = "black";
+            play.style.top = "0";   
+        })  
     }
 })
 // adocument.getElementById('btn').onclick = function(){
